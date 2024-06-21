61f765242edcb43e1f708543fa94856fa5d65550
Mateus Felipe <brasil.mateus748@gmail.com>
2024-06-21 18:15:06 -0300

    Initial commit

---
+++ src/ContaPoupança.java
@@ -0,0 +1,29 @@
+public class ContaPoupança {
+    private int agencia;
+    private int conta;
+    private double saldo;
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getConta() {
+        return conta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void sacar() {
+
+    }
+
+    public void depositar() {
+
+    }
+
+    public void transferir() {
+
+    }
+}
